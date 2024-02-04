3f2439043d294e6695e505b62844e93b45e37657
Bruno Costa <bruno@rhizom.me>
2024-02-03 23:46:10 -0300

    feat: create opening and more

--- handler/handler.go
+++ handler/handler.go
@@ -3,13 +3,42 @@ package handler
 import (
 	"net/http"
 
+	"github.com/brunomc/api-go/config"
+	"github.com/brunomc/api-go/schemas"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "opening",
-	})
+	request := CreateOpeningRequest{}
+	ctx.BindJSON(&request)
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error %v", err.Error())
+		sendError(ctx, 400, err.Error())
+		return
+	}
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Link:     request.Link,
+		Remote:   *request.Remote,
+		Salary:   request.Salary,
+	}
+	if err := db.Create(&opening).Error; err != nil {
+		sendError(ctx, 400, err.Error())
+		return
+	}
+	sendSuccess(ctx, "create-opening", opening)
 }
 func ShowOpeningHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
